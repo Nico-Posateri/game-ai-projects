083a35262ca11acabe73975c86188ee62928a653
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-12-29 14:58:10 -0600

    Update basic-behavior-tree.cs

--- basic-behavior-tree.cs
+++ basic-behavior-tree.cs
@@ -1,4 +1,4 @@
 // A "zoomed-in" look at the theoretical BT of an NPC.
-// The functions used in this Behavior Tree do not exist as a result.
+// Some of the functions used in this Behavior Tree do not exist as a result.
 // This is simply meant to showcase the basic elements of a Behavior Tree.
 //
