42078dd19497abda6a91478cd5829a527662bba4
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-12-29 14:57:55 -0600

    Create basic-pathfinding.cs

---
+++ basic-pathfinding.cs
@@ -0,0 +1,4 @@
+// A "zoomed-in" look at the theoretical pathfinding of an NPC.
+// Some of the functions used in this pathfinding algorithm do not exist as a result.
+// This is simply meant to showcase the basic elements of pathfinding.
+//
