7a65ea31bf10797f4a0f32746ba8537cf2a42e33
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2025-01-02 16:22:50 -0600

    Update behavior-steering-and-emergence.cs

--- behavior-steering-and-emergence.cs
+++ behavior-steering-and-emergence.cs
@@ -2,7 +2,7 @@
 // Some of the elements referenced in these algorithms do not exist as a result.
 // This is simply meant to showcase the basic elements of emergent behavior and behavior steering, courtesy of Alan Zucconi.
 
-// Steering ////////////////////////////////////////////////////////////////
+// Seeking ////////////////////////////////////////////////////////////////
 
 public Rigidbody Rigidbody;
 public Transform Target;
@@ -12,5 +12,16 @@ public float MaxForce;
 
 void FixedUpdate ()
 {
-    // ...
+    Vector3 P = transform.position;
+    Vector3 T = Target.position;
+
+    Vector3 U_hat = (T - P).normalized;
+    Vector3 U = U_hat * Speed;
+
+    Vector3 V = Rigidbody.velocity;
+    Vector3 dV = U - V;
+    Vector3 F = Vector3.ClampMagnitude(dV, MaxForce);
+    Rigidbody.AddForce(F);
 }
+
+// Fleeing ////////////////////////////////////////////////////////////////
