968db6f9095d8a59bce5ed9f3a817243ccc4af84
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2025-01-02 14:53:07 -0600

    Update

---
+++ behavior-steering-and-emergence.cs
@@ -0,0 +1,16 @@
+// A "zoomed-in" look at the theoretical behavior steering and emergent behaviors of an NPC.
+// Some of the elements referenced in these algorithms do not exist as a result.
+// This is simply meant to showcase the basic elements of emergent behavior and behavior steering, courtesy of Alan Zucconi.
+
+// Steering ////////////////////////////////////////////////////////////////
+
+public Rigidbody Rigidbody;
+public Transform Target;
+
+public float Speed;
+public float MaxForce;
+
+void FixedUpdate ()
+{
+    // ...
+}
