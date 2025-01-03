adc37458ac3728da1c123e08e40f103aa5b2f92b
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2025-01-03 01:48:06 -0600

    Update behavior-steering-and-emergence.cs

--- behavior-steering-and-emergence.cs
+++ behavior-steering-and-emergence.cs
@@ -2,26 +2,52 @@
 // Some of the elements referenced in these algorithms do not exist as a result.
 // This is simply meant to showcase the basic elements of emergent behavior and behavior steering, courtesy of Alan Zucconi.
 
-// Seeking ////////////////////////////////////////////////////////////////
+/* BEHAVIOR STEERING *//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-public Rigidbody Rigidbody;
-public Transform Target;
+// Seeking
 
-public float Speed;
-public float MaxForce;
+public Rigidbody Rigidbody; // The rigid body object we will move
+public Transform Target; // The target we want the rigid body to go to
 
-void FixedUpdate ()
+public float Speed; // The target speed of the object
+public float MaxForce; // How much force can be applied to the object
+
+void FixedUpdate () // In Unity, FixedUpdate allows continuous alteration of physics objects
 {
-    Vector3 P = transform.position;
-    Vector3 T = Target.position;
+    Vector3 P = transform.position; // P: Current position
+    Vector3 T = Target.position; // T: Target position
 
-    Vector3 U_hat = (T - P).normalized;
-    Vector3 U = U_hat * Speed;
+    Vector3 U_hat = (T - P).normalized; // Ū: Velocity direction (the P and T are simply flipped between Seek and Flee)
+    Vector3 U = U_hat * Speed; // U: Desired velocity
 
-    Vector3 V = Rigidbody.velocity;
-    Vector3 dV = U - V;
-    Vector3 F = Vector3.ClampMagnitude(dV, MaxForce);
-    Rigidbody.AddForce(F);
+    // Version 2
+    Vector3 V = Rigidbody.velocity; // Extracts the current velocity from the rigid body
+    Vector3 dV = U - V; // Calculate delta V
+    Vector3 F = Vector3.ClampMagnitude(dV, MaxForce); // Calculate F
+    Rigidbody.AddForce(F); // Add the force to the rigid body, should multiply by delta time in Unity
 }
 
-// Fleeing ////////////////////////////////////////////////////////////////
+// Fleeing
+
+public Rigidbody Rigidbody; // The rigid body object we will move
+public Transform Target; // The target we want the rigid body to go to
+
+public float Speed; // The target speed of the object
+public float MaxForce; // How much force can be applied to the object
+
+void FixedUpdate () // In Unity, FixedUpdate allows continuous alteration of physics objects
+{
+    Vector3 P = transform.position; // P: Current position
+    Vector3 T = Target.position; // T: Target position
+
+    Vector3 U_hat = (P - T).normalized; // Ū: Velocity direction (the P and T are simply flipped between Seek and Flee)
+    Vector3 U = U_hat * Speed; // U: Desired velocity
+
+    // Version 2
+    Vector3 V = Rigidbody.velocity; // Extracts the current velocity from the rigid body
+    Vector3 dV = U - V; // Calculate delta V
+    Vector3 F = Vector3.ClampMagnitude(dV, MaxForce); // Calculate F
+    Rigidbody.AddForce(F); // Add the force to the rigid body, should multiply by delta time in Unity
+}
+
+/* EMERGENT BEHAVIORS */////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
