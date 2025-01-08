856ad8ae5bcc1ff443da5dd3084d3d0ceace0f61
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2025-01-08 11:06:55 -0600

    Update basic-utility-curve.cs

--- basic-utility-curve.cs
+++ basic-utility-curve.cs
@@ -1,1 +1,13 @@
-// A "zoomed-in" look at the animation curve of an AI agent.
+// A "zoomed-in" look at the animation curve of an AI agent, a component designed for Unity, courtesy of Alan Zucconi.
+
+public class Agent : MonoBehaviour
+{
+    public float Health;
+    public AnimationVurve Curve; // Produces a box in the inspector called "Health" which displayes the utility curve for an agent's health
+
+    public void Update ()
+    {
+        float utility = Curve.Evaluate(Health);
+        Debug.Log(utility);
+    }
+}
